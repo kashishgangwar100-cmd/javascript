9b3f7da0da4efbc104ccce3bbf2ba13370b66619
Kashish Gangwar <kashishgangwar100@gmail.com>
2026-06-26 18:34:11 +0000

    Add JavaScript numbers and math examples

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,42 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));//gives 2 values after decimal
+
+// const otherNumber = 23.8966//result 23.9
+const otherNumber= 123.8966//result is 124
+
+
+// console.log(otherNumber.toPrecision(3));//returns a string
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+ 
+//+++++Maths+++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));//gives absolute values return 4,positive number remains the same but negative one becomes positive
+
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+console.log(Math.floor(Math.random()*10) + 1);//value is always between 0 to 1
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
+
+
