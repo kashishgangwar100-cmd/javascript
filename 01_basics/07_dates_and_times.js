f51d637740261e8ae1d57578c395d7ddcbd2832b
Kashish Gangwar <kashishgangwar100@gmail.com>
2026-06-27 18:10:50 +0000

    date and time

---
+++ 01_basics/07_dates_and_times.js
@@ -0,0 +1,34 @@
+//Date
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString);
+// console.log(myDate.toLocaleString());
+
+// console.log(typeOf myDate);
+
+// let myCreatedDate = new Date(2023,0,23)
+// let myCreatedDate = new Date(2023,0,23,5,3)
+
+let myCreatedDate = new Date("01-12-2023")
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleDateString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());//result in seconds
+// console.log(Math.floor(Date.now()/1000));//result is in milisecond
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time`
+
+
+
+
+
+
+
