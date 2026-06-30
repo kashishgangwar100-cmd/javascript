6c095ae0876ba7620a82f1aa363bea46e771c921
Kashish Gangwar <kashishgangwar100@gmail.com>
2026-06-30 14:32:31 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,7 +1,7 @@
 // const tinderUser = new Object() this is a singleton object
 const tinderUser = {} //non singleton object
 
-tinderUse.id = "123abc";
+tinderUser.id = "123abc";
 tinderUser.name = "kashish"
 tinderUser.isLoggedIn = false
 
@@ -17,8 +17,8 @@ const regularUser ={
         }
     }
 }
-console.log(regularUser.fullname);
-console.log(regularUser.fullname?.userfullname.firstname);
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname?.userfullname.firstname);
 
 const obj1 = {1:"a",2:"b"}
 const obj2 = {3:"a",4:"b"}
@@ -44,14 +44,46 @@ const users = [
         email: "k@gmail.com" 
     }
 ]
-users[1].email;
-console.log(tinderUser);
+// users[1].email;
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));//put all keys in array//important for database
+// console.log(Object.keys(tinderUser));//put all keys in array//important for database
 
-console.log(Objects.values(tinderUser));
-console.log(Object.entries(tinderUser));//less in use
+// console.log(Objects.values(tinderUser));
+// console.log(Object.entries(tinderUser));//less in use
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "kashish"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: Instructor} = course
+// console.log(courseInstructor); 
+console.log(instructor);
+
+// const navbar =({company}) =>{
+    
+
+// }
+// navbar(company = "kashish") objcct deconstructor
+
+//in old time values are in form of XML structure which is very complex now value is in JSON structure
+
+// {
+//     "name" : "kashish",
+//      "coursename" : "java in hindi",
+//     "price":"free"
+// }almost treated as JSON
+
+// [
+//     {},
+//     {},
+//     {}
+// ]//object is send in using JSON format
 
