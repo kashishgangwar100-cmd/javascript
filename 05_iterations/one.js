a29c1d6539f667532e829ce5608a287ce5ea1e60
Kashish Gangwar <kashishgangwar100@gmail.com>
2026-07-05 07:39:09 +0000

    loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,38 @@
+//for loops
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element == 5){
+        console.log("5 is the best number");
+    }
+    console.log(element);
+}
+
+// console.log(element);//cant access element from here
+
+for (let i = 0; i < 10;i++) {
+    console.log(`outer loop value: ${i}`);
+  for( let j = 0; j <= 10 ;j++){
+    // console.log(`Inner loop value ${j} and inner loop ${i}`);
+    // console.log(i + '*' + j + '=' + i*j);
+  }
+}
+
+let myArray = ["flash", "batman" , "superman"]
+// console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+
+//break and continue
+
+for (let index = 0; index <= 20; index++) {
+    if(index == 5){
+        console.log('Detected 5');
+        // break
+        continue
+    }
+console.log(`Value of i is ${index}`);    
+}
