a53d59424c2a7a534a22f732baaaac07217cb0a6
Kashish Gangwar <kashishgangwar100@gmail.com>
2026-07-12 18:27:19 +0000

    filter map

---
+++ 05_iterations/six.js
@@ -0,0 +1,29 @@
+// const coding = ["js", "ruby", "java"]
+
+// const values = coding.forEach((item) => {
+//     console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+myNums.filter((num) => num > 4 )//return values
+
+// const newNums = myNums.filter((num) =>{
+    // return num > 4
+// })
+const newNums = []
+
+myNums.forEach((num) => {
+    if(num > 4){
+        newNums.push(num)
+    }
+})
+
+console.log(newNums);
+
+//const userBooks = books.filter((bk) => bk.genre == 'history')
+//console.log(userBooks);
+
